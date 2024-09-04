9647337d7dc09b1ab7f7fa99678c44dd6b7188c4
codemmk <2107228359@qq.com>
2024-09-04 20:23:41 +0800

    图片展示

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,21 @@ console.log(data.value)
 </script>
 
 <template>
-  <div class="home">
+  <div class="home p-8">
     <a-button type="primary" @click="navigateTo('add')">导入图片</a-button>
+    <div class="mt-8">
+      <div>
+        <div class="p-2" v-for="item in data" :key="item.name">
+          <h2 class="mb-4">{{ item.name }}</h2>
+          <div class="grid grid-cols-3 gap-4">
+            <div class="w-full h-64 bg-gray-200 rounded-md overflow-hidden relative" v-for="i in item.images" :key="i.url">
+              <a-image class="w-full h-full object-cover" :src="i.url" />
+              <div class="absolute bottom-0 left-0 p-2 bg-black bg-opacity-50 text-white">{{ i.name }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
